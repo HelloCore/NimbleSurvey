acca2b55f29f33658409f9479036fc07d4fa95d2
AKKHARAWAT CHAYAPIWAT <aukwat@hotmail.com>
2017-02-23 21:46:07 +0700

    Initial commit

---
+++ NimbleSurvey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NimbleSurvey
+//
+//  Created by AKKHARAWAT CHAYAPIWAT on 2/23/17.
+//  Copyright © 2017 AKKHARAWAT CHAYAPIWAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
